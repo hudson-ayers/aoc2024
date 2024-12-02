9ed47bc5ddf9ace780024164a00cc90a30b9a29e
Hudson Ayers <hayers@stanford.edu>
2024-12-01 19:57:23 -0500

    day 1 part 2
    Signed-off-by: Hudson Ayers <hayers@stanford.edu>

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::HashMap;
 use std::fs;
 
 fn parse_line(s: &str) -> (i32, i32) {
@@ -8,9 +9,7 @@ fn parse_line(s: &str) -> (i32, i32) {
     )
 }
 
-// could use bufreader to optimize large file reads,
-// but input is only 1000 lines so no point
-fn main() -> Result<(), std::io::Error> {
+fn part_1() -> Result<(), std::io::Error> {
     let s = fs::read_to_string("src/input.txt")?;
     let mut l1 = Vec::new();
     let mut l2 = Vec::new();
@@ -29,5 +28,29 @@ fn main() -> Result<(), std::io::Error> {
         .map(|(&a, &b)| i32::abs(a - b))
         .sum(); // O(n)
     println!("Difference: {difference}");
+    Ok(())
+}
+
+fn part_2() -> Result<(), std::io::Error> {
+    let s = fs::read_to_string("src/input.txt")?;
+    let mut l1 = Vec::new();
+    let mut h2 = HashMap::new();
+    s.lines()
+        .map(parse_line)
+        .map(|(a, b)| {
+            l1.push(a);
+            h2.insert(b, h2.get(&b).unwrap_or(&0i32) + 1)
+        })
+        .count(); // O(n)
+    let similarity: i32 = l1.iter().map(|a| h2.get(a).unwrap_or(&0) * a).sum(); // O(n)
+    println!("Similarity: {similarity}");
+    Ok(())
+}
+
+// Could use bufreader to optimize large file reads,
+// but input is only 1000 short lines so no point
+fn main() -> Result<(), std::io::Error> {
+    part_1()?;
+    part_2()?;
     Ok(())
 }
